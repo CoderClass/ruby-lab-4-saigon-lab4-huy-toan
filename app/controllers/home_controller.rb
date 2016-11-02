0e593a42d0d9771fe4847501c2aa64023b9845f5
Huy Pham <huyphamthanh8290@gmail.com>
2016-11-02 20:34:58 +0700

    Milestone 3: finish

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def logout
+    session[:username] = nil
+    flash[:success] = 'Log out success!'
+    redirect_back fallback_location: root_path
+  end
+
+  def login
+    session[:username] = params[:user][:name]
+    flash[:success] = 'Login success!'
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'logout' => 'home#logout'
+  post 'login' => 'home#login'
+
   get 'messages/index'
 
   get 'messages/create'
